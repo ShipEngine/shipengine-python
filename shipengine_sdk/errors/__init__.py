475061dbd4760df5bf24fa389ce49a18af6f5d19
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-20 17:16:43 -0500

    work in progress - create exception classes

--- shipengine_sdk/errors/__init__.py
+++ shipengine_sdk/errors/__init__.py
@@ -1,1 +1,37 @@
-"""Initial Docstring."""
+"""Exceptions that will be raised through-out the ShipEngine SDK."""
+
+
+class ShipEngineException(Exception):
+    """Base exception class that all other client exceptions will inherit from."""
+
+
+class AccountStatusException(ShipEngineException):
+    """An exception that indicates there is an issue with a given account's status."""
+
+
+class BusinessRuleException(ShipEngineException):
+    """An exception that indicates business rule logic has been violated."""
+
+
+class SecurityException(ShipEngineException):
+    """This exception will be raised in the event that authorization is failed."""
+
+
+class SystemException(ShipEngineException):
+    """This exception will be thrown on 500 server responses and fatal errors."""
+
+
+class ValidationException(ShipEngineException):
+    """An exception that indicates a given value does not match it's required type."""
+
+
+class TimeoutException(ShipEngineException):
+    """An exception that indicates the configured timeout has been reached for a given request."""
+
+
+class InvalidFieldValueException(ShipEngineException):
+    """"""
+
+
+class RateLimitExceededException(ShipEngineException):
+    """"""
--- tests/test_shipengine_sdk.py
+++ tests/test_shipengine_sdk.py
@@ -4,4 +4,4 @@ from shipengine_sdk import __version__
 
 def test_version():
     """Test the package version of the ShipEngine SDK."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.0.1"
