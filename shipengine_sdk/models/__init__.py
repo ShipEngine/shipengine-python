118516650381a779e3bef17a0d14df0ca2e29aab
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-21 16:58:05 -0500

    Acceptance criteria per DX-1438 - create error classes

--- shipengine_sdk/models/__init__.py
+++ shipengine_sdk/models/__init__.py
@@ -1,2 +1,4 @@
 """ShipEngine SDK Models & Enumerations"""
 from shipengine_sdk.models.enums import error_code
+from shipengine_sdk.models.enums import error_source
+from shipengine_sdk.models.enums import error_type
