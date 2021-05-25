591d63ec29624cfb26186e2345f6ccc6e06d755b
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-25 13:59:14 -0500

    Acceptance criteria per DX-1441 - empty API Key provided

--- tests/test_shipengine.py
+++ tests/test_shipengine.py
@@ -2,15 +2,24 @@
 import pytest
 
 from shipengine_sdk import __version__
-from shipengine_sdk import ShipEngineConfig
+from shipengine_sdk import ShipEngine
 from shipengine_sdk.errors import ValidationError
-from shipengine_sdk.models import ErrorCode
-from shipengine_sdk.models import ErrorSource
-from shipengine_sdk.models import ErrorType
+from shipengine_sdk.util.sdk_assertions import api_key_validation_error_assertions
 
 
 def shipengine_no_api_key():
-    return ShipEngineConfig(dict(retries=2))
+    """Return an error from no API Key."""
+    return ShipEngine(dict(retries=2))
+
+
+def shipengine_empty_api_key():
+    """Return an error from empty API Key."""
+    return ShipEngine(config="")
+
+
+def shipengine_whitespace_in_api_key():
+    """Return an error from whitespace in API Key."""
+    return ShipEngine(config=" ")
 
 
 class TestShipEngine:
@@ -19,15 +28,19 @@ class TestShipEngine:
         assert __version__ == "0.0.1"
 
     def test_no_api_key_provided(self) -> None:
-        """DX-1440 - No API Key at instantiation"""
+        """DX-1440 - No API Key at instantiation."""
         try:
             shipengine_no_api_key()
         except ValidationError as e:
             with pytest.raises(ValidationError):
                 shipengine_no_api_key()
-            assert type(e) is ValidationError
-            assert e.request_id is None
-            assert e.error_type is ErrorType.VALIDATION.value
-            assert e.error_code is ErrorCode.FIELD_VALUE_REQUIRED.value
-            assert e.source is ErrorSource.SHIPENGINE.value
-            assert e.message == "A ShipEngine API key must be specified."
+            api_key_validation_error_assertions(e)
+
+    def test_empty_api_key_provided(self):
+        """DX-1441 - Empty API Key at instantiation."""
+        try:
+            shipengine_empty_api_key()
+        except ValidationError as e:
+            with pytest.raises(ValidationError):
+                shipengine_empty_api_key()
+            api_key_validation_error_assertions(e)
--- tests/test_shipengine_config.py
+++ tests/test_shipengine_config.py
@@ -6,14 +6,26 @@ from shipengine_sdk.errors import ValidationError
 from shipengine_sdk.models import ErrorCode
 from shipengine_sdk.models import ErrorSource
 from shipengine_sdk.models import ErrorType
+from shipengine_sdk.util import api_key_validation_error_assertions
 
 
 def config_with_no_api_key():
+    """Return an error from no API Key."""
     return ShipEngineConfig(dict(retries=2))
 
 
+def config_with_empty_api_key():
+    """Return an error from empty API Key."""
+    return ShipEngineConfig(dict(api_key=""))
+
+
+def config_with_whitespace_in_api_key():
+    """Return an error from whitespace in API Key."""
+    return ShipEngineConfig(dict(api_key=" "))
+
+
 class TestShipEngineConfig:
-    def test_no_api_key(self):
+    def test_no_api_key_provided(self):
         """DX-1440 - No API Key at instantiation"""
         try:
             config_with_no_api_key()
@@ -26,3 +38,12 @@ class TestShipEngineConfig:
             assert e.error_code is ErrorCode.FIELD_VALUE_REQUIRED.value
             assert e.source is ErrorSource.SHIPENGINE.value
             assert e.message == "A ShipEngine API key must be specified."
+
+    def test_empty_api_key_provided(self):
+        """DX-1441 - Empty API Key at instantiation."""
+        try:
+            config_with_empty_api_key()
+        except ValidationError as e:
+            api_key_validation_error_assertions(e)
+            with pytest.raises(ValidationError):
+                config_with_empty_api_key()
