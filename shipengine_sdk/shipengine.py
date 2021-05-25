bbc07f52978206f95514a1d060eb76317de89c06
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-25 10:57:09 -0500

    Introduced the ShipEngine object and added ShipEngineConfig to it - you can pass in a string api_key or a dictionary of configuration options. Api Key is required.

--- shipengine_sdk/shipengine.py
+++ shipengine_sdk/shipengine.py
@@ -1,5 +1,27 @@
-"""Exposes the functionality of the ShipEngine API."""
+"""The entrypoint to the ShipEngine API SDK."""
+from typing import Dict
+from typing import Union
+
+from shipengine_sdk import ShipEngineConfig
 
 
 class ShipEngine:
-    pass
+    config: ShipEngineConfig
+    """
+    Global configuration for the ShipEngine API client, such as timeouts,
+    retries, page size, etc. This configuration applies to all method calls,
+    unless specifically overridden when calling a method.
+    """
+
+    def __init__(self, config: Union[str, Dict[str, any]]) -> None:
+        """
+        Exposes the functionality of the ShipEngine API.
+
+        The `api_key` you pass in can be either a ShipEngine sandbox
+        or production API Key. (sandbox keys start with "TEST_")
+        """
+
+        if type(config) is str:
+            self.config = ShipEngineConfig({"api_key": config})
+        elif type(config) is dict:
+            self.config = ShipEngineConfig(config)
