6fd9985afd5d5adf52f18dd87a73e8fcc3654434
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-25 10:52:08 -0500

    Added imports for ShipEngine & ShipEngineConfig :snake:

--- shipengine_sdk/__init__.py
+++ shipengine_sdk/__init__.py
@@ -6,5 +6,7 @@ import logging
 from logging import NullHandler
 
 # SDK imports here
+from .shipengine import ShipEngine
+from .shipengine_config import ShipEngineConfig
 
 logging.getLogger(__name__).addHandler(NullHandler())
