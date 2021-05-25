b362e8e920f90096373134628e5736f338ae248b
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-25 16:24:11 -0500

    Acceptance criteria per DX-1442 - Invalid retries at instantiation

--- tests/test_shipengine_config.py
+++ tests/test_shipengine_config.py
@@ -2,7 +2,8 @@
 import pytest
 
 from shipengine_sdk import ShipEngineConfig
-from shipengine_sdk.errors import ValidationError
+from shipengine_sdk.errors import InvalidFieldValueError, ValidationError
+from shipengine_sdk.models import ErrorCode, ErrorSource, ErrorType
 from shipengine_sdk.util import api_key_validation_error_assertions
 
 
@@ -21,12 +22,17 @@ def config_with_whitespace_in_api_key():
     return ShipEngineConfig(dict(api_key=" "))
 
 
+def config_with_invalid_retries():
+    """Return an error from an invalid retry value being passed in"""
+    return ShipEngineConfig(dict(api_key="baz", retries=-3))
+
+
 class TestShipEngineConfig:
     def test_no_api_key_provided(self) -> None:
         """DX-1440 - No API Key at instantiation"""
         try:
             config_with_no_api_key()
-        except ValidationError as e:
+        except Exception as e:
             api_key_validation_error_assertions(e)
             with pytest.raises(ValidationError):
                 config_with_no_api_key()
@@ -35,7 +41,21 @@ class TestShipEngineConfig:
         """DX-1441 - Empty API Key at instantiation."""
         try:
             config_with_empty_api_key()
-        except ValidationError as e:
+        except Exception as e:
             api_key_validation_error_assertions(e)
             with pytest.raises(ValidationError):
                 config_with_empty_api_key()
+
+    def test_invalid_retries_provided(self):
+        """DX-1442 - Invalid retries at instantiation."""
+        try:
+            config_with_invalid_retries()
+        except InvalidFieldValueError as e:
+            assert type(e) is InvalidFieldValueError
+            assert e.request_id is None
+            assert e.error_type is ErrorType.VALIDATION.value
+            assert e.error_code is ErrorCode.INVALID_FIELD_VALUE.value
+            assert e.source is ErrorSource.SHIPENGINE.value
+            assert e.message == "retries - Retries must be zero or greater."
+            with pytest.raises(InvalidFieldValueError):
+                config_with_invalid_retries()
