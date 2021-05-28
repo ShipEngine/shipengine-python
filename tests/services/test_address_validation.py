8330aca5cb0c61a51e58b8da2866201c4edbfa7f
KaseyCantu <kpc.software.engineer@gmail.com>
2021-05-28 16:30:52 -0500

    Acceptance criteria per DX-1025 - Valid commercial address.

--- tests/services/test_address_validation.py
+++ tests/services/test_address_validation.py
@@ -33,12 +33,29 @@ def valid_residential_address() -> Address:
     )
 
 
+def valid_commercial_address() -> Address:
+    """
+    Return a test Address object with valid commercial
+    address information.
+    """
+    return Address(
+        street=["4 Jersey St", "ste 200"],
+        city_locality="Boston",
+        state_province="MA",
+        postal_code="02215",
+        country_code="US",
+    )
+
+
+def validate_an_address(address: Address) -> AddressValidateResult:
+    return stub_shipengine_instance().validate_address(address=address)
+
+
 class TestValidateAddress:
     def test_valid_residential_address(self):
-        """DX-1024 - Valid residential address"""
-        shipengine = stub_shipengine_instance()
-        valid_address = valid_residential_address()
-        validated_address = shipengine.validate_address(valid_address)
+        """DX-1024 - Valid residential address."""
+        residential_address = valid_residential_address()
+        validated_address = validate_an_address(residential_address)
         address = validated_address.normalized_address
 
         assert type(validated_address) is AddressValidateResult
@@ -46,10 +63,34 @@ class TestValidateAddress:
         assert address is not None
         assert (
             address.street[0]
-            == (valid_address.street[0] + " " + valid_address.street[1]).replace(".", "").upper()
+            == (residential_address.street[0] + " " + residential_address.street[1])
+            .replace(".", "")
+            .upper()
         )
-        assert address.city_locality == valid_address.city_locality.upper()
-        assert address.state_province == valid_address.state_province.upper()
-        assert address.postal_code == valid_address.postal_code
-        assert address.country_code == valid_address.country_code.upper()
+        assert address.city_locality == residential_address.city_locality.upper()
+        assert address.state_province == residential_address.state_province.upper()
+        assert address.postal_code == residential_address.postal_code
+        assert address.country_code == residential_address.country_code.upper()
         assert address.is_residential is True
+
+    def test_valid_commercial_address(self):
+        """DX-1025 - Valid commercial address."""
+        commercial_address = valid_commercial_address()
+        validated_address = validate_an_address(commercial_address)
+        address = validated_address.normalized_address
+
+        assert type(validated_address) is AddressValidateResult
+        assert validated_address.is_valid is True
+        assert len(validated_address.messages) == 0
+        assert address is not None
+        assert (
+            address.street[0]
+            == (commercial_address.street[0] + " " + commercial_address.street[1])
+            .replace(".", "")
+            .upper()
+        )
+        assert address.city_locality == commercial_address.city_locality.upper()
+        assert address.state_province == commercial_address.state_province.upper()
+        assert address.postal_code == commercial_address.postal_code
+        assert address.country_code == commercial_address.country_code.upper()
+        assert address.is_residential is False
