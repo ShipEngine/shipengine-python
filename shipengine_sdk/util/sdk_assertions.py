b4300f79c9a881f31039ccf8d1220a592c16b197
KaseyCantu <kpc.software.engineer@gmail.com>
2021-06-04 14:49:27 -0500

    Added an extra error check for the 404 as there are cases where a 404 will occur, but our jsonrpc server does not have the error object present.

--- shipengine_sdk/util/sdk_assertions.py
+++ shipengine_sdk/util/sdk_assertions.py
@@ -7,6 +7,7 @@ from ..errors import (
     ClientTimeoutError,
     InvalidFieldValueError,
     RateLimitExceededError,
+    ShipEngineError,
     ValidationError,
 )
 from ..models.enums import Country, ErrorCode, ErrorSource, ErrorType
@@ -173,19 +174,25 @@ def timeout_validation_error_assertions(error) -> None:
     assert error.source is ErrorSource.SHIPENGINE.value
 
 
-def is_response_404(status_code: int, response_body: Dict[str, any]) -> None:
+def is_response_404(status_code: int, response_body: Dict[str, any], config) -> None:
     """Check if status_code is 404 and raises an error if so."""
-    if "error" in response_body:
+    if "error" in response_body and status_code == 404:
         error = response_body["error"]
         error_data = error["data"]
-        if status_code == 404:
-            raise ClientSystemError(
-                message=error["message"],
-                request_id=response_body["id"],
-                source=error_data["source"],
-                error_type=error_data["type"],
-                error_code=error_data["code"],
-            )
+        raise ClientSystemError(
+            message=error["message"],
+            request_id=response_body["id"],
+            source=error_data["source"],
+            error_type=error_data["type"],
+            error_code=error_data["code"],
+        )
+    elif status_code == 404:
+        raise ShipEngineError(
+            message=f"Resource not found, please check the base_uri you have set and try again. [{config.base_uri}] is currently set.",  # noqa
+            source=ErrorSource.SHIPENGINE.value,
+            error_type=ErrorType.SYSTEM.value,
+            error_code=ErrorCode.NOT_FOUND.value,
+        )
 
 
 def is_response_429(status_code: int, response_body: Dict[str, any], config) -> None:
