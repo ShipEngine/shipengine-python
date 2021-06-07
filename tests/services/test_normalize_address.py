902308e2a3bb857dddbdf0619fb9772c042dd2fb
KaseyCantu <kpc.software.engineer@gmail.com>
2021-06-07 15:29:11 -0500

    Acceptance criteria per DX-1042 - Normalize valid commercial address. https://auctane.atlassian.net/browse/DX-1042

--- tests/services/test_normalize_address.py
+++ tests/services/test_normalize_address.py
@@ -4,6 +4,7 @@ from shipengine_sdk.models import Address
 from ..util.test_data import (
     normalize_an_address,
     us_valid_normalize_assertions,
+    valid_commercial_address,
     valid_residential_address,
 )
 
@@ -20,3 +21,14 @@ class TestNormalizeAddress:
             expected_residential_indicator=True,
         )
         assert type(normalized) is Address
+
+    def test_normalize_valid_commercial_address(self) -> None:
+        """DX-1042 - Normalize valid commercial address."""
+        commercial_address = valid_commercial_address()
+        normalized = normalize_an_address(commercial_address)
+
+        us_valid_normalize_assertions(
+            original_address=commercial_address,
+            normalized_address=normalized,
+            expected_residential_indicator=False,
+        )
