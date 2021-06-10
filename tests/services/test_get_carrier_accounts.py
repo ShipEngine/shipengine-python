60b52184ee2067ce0553d67610f3024b7f101d36
KaseyCantu <kpc.software.engineer@gmail.com>
2021-06-10 09:41:13 -0500

    Fixed order of tests to match ticket number order.

--- tests/services/test_get_carrier_accounts.py
+++ tests/services/test_get_carrier_accounts.py
@@ -6,22 +6,6 @@ from ..util.test_helpers import stub_get_carrier_accounts
 
 
 class TestGetCarrierAccounts:
-    def test_multiple_accounts_of_same_carrier(self):
-        """DX-1077 - Multiple accounts of the same carrier."""
-        accounts = stub_get_carrier_accounts()
-
-        assert len(accounts) == 5
-        assert accounts[0]["carrier"]["code"] == Carriers.UPS.value
-        assert accounts[0]["account_id"] != accounts[1]["account_id"]
-        assert accounts[1]["carrier"]["code"] == Carriers.FEDEX.value
-        assert accounts[2]["carrier"]["code"] == Carriers.FEDEX.value
-        assert accounts[3]["carrier"]["code"] == Carriers.USPS.value
-        assert accounts[4]["carrier"]["code"] == Carriers.STAMPS_COM.value
-
-        for account in accounts:
-            assert account["account_id"].startswith("car_")
-            assert account["name"] is not None
-
     def test_no_accounts_setup(self) -> None:
         """DX-1075 - No accounts setup yet."""
         accounts = stub_get_carrier_accounts(carrier_code="sendle")
@@ -49,3 +33,19 @@ class TestGetCarrierAccounts:
             assert type(account["carrier"]["code"]) == str
             assert account["carrier"]["name"] is not None
             assert type(account["carrier"]["name"]) == str
+
+    def test_multiple_accounts_of_same_carrier(self):
+        """DX-1077 - Multiple accounts of the same carrier."""
+        accounts = stub_get_carrier_accounts()
+
+        assert len(accounts) == 5
+        assert accounts[0]["carrier"]["code"] == Carriers.UPS.value
+        assert accounts[0]["account_id"] != accounts[1]["account_id"]
+        assert accounts[1]["carrier"]["code"] == Carriers.FEDEX.value
+        assert accounts[2]["carrier"]["code"] == Carriers.FEDEX.value
+        assert accounts[3]["carrier"]["code"] == Carriers.USPS.value
+        assert accounts[4]["carrier"]["code"] == Carriers.STAMPS_COM.value
+
+        for account in accounts:
+            assert account["account_id"].startswith("car_")
+            assert account["name"] is not None
