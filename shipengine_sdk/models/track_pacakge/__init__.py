c7587d007d5923d0ec2be8a69e5ae01b58d3c085
KaseyCantu <kpc.software.engineer@gmail.com>
2021-06-11 14:38:57 -0500

    Models necessary for Track Package implementation.

--- shipengine_sdk/models/track_pacakge/__init__.py
+++ shipengine_sdk/models/track_pacakge/__init__.py
@@ -1,5 +1,5 @@
 """Data objects to be used in the `track_package` and `track` methods."""
-
+import json
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Dict, List, Optional
@@ -50,9 +50,9 @@ class Shipment:
         self.actual_delivery_date = actual_delivery_date
 
     def _get_carrier_account(self, carrier: str, account_id: str) -> CarrierAccount:
-        get_accounts = GetCarrierAccounts()
-        target_carrier = list()
-        carrier_accounts = get_accounts.fetch_cached_carrier_accounts(
+        get_accounts: GetCarrierAccounts = GetCarrierAccounts()
+        target_carrier: List[CarrierAccount] = list()
+        carrier_accounts: List[CarrierAccount] = get_accounts.fetch_cached_carrier_accounts(
             carrier_code=carrier, config=self.config
         )
 
@@ -65,6 +65,12 @@ class Shipment:
                     message=f"accountID [{account_id}] doesn't amtch any of the accounts connected to your "
                     + "ShipEngine Account."
                 )
+
+    def to_dict(self):
+        return (lambda o: o.__dict__)(self)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=2)
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
@@ -88,8 +94,91 @@ class TrackingQuery:
     tracking_number: str
 
 
+@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
+class Location:
+    city_locality: Optional[str]
+    state_province: Optional[str]
+    postal_code: Optional[str]
+    country_code: Optional[str]
+    latitude: Optional[float]
+    longitude: Optional[float]
+
+
+class TrackingEvent:
+    date_time: datetime
+    carrier_date_time: datetime
+    status: str
+    description: Optional[str]
+    carrier_status_code: Optional[str]
+    carrier_detail_code: Optional[str]
+    signer: Optional[str]
+    location: Optional[Location]
+
+    def __init__(self, event: Dict[str, any]) -> None:
+        """Tracking event object."""
+        self.date_time = IsoString(iso_string=event["timestamp"]).to_datetime_object()
+
+        self.carrier_date_time = IsoString(
+            iso_string=event["carrierTimestamp"]
+        ).to_datetime_object()
+
+        self.status = event["status"]
+        self.description = event["description"] if "description" in event else None
+        self.carrier_status_code = (
+            event["carrierStatusCode"] if "carrierStatusCode" in event else None
+        )
+        self.signer = event["signer"] if "signer" in event else None
+        self.location = Location.from_dict(event["location"]) if "location" in event else None
+
+    def to_dict(self):
+        return (lambda o: o.__dict__)(self)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=2)
+
+
 class TrackPackageResult:
     shipment: Optional[Shipment]
     package: Optional[Package]
-    events: Optional[List[str]]
+    events: Optional[List[TrackingEvent]]
+
+    def __init__(self, api_response: Dict[str, any], config: ShipEngineConfig) -> None:
+        """This object is used as the return type for the `track_package` and `track` methods."""
+        result = api_response["result"]
+        for event in result["events"]:
+            self.events.append(TrackingEvent(event=event))
+
+        self.shipment = (
+            Shipment(
+                shipment=result["shipment"],
+                actual_delivery_date=self.get_latest_event().date_time,
+                config=config,
+            )
+            if "shipment" in api_response
+            else None
+        )
+        self.package = Package.from_dict(result["package"]) if "package" in result else None
+
+    def get_errors(self) -> List[TrackingEvent]:  # TODO: debug
+        """Returns **only** the EXCEPTION events."""
+        errors: List[TrackingEvent] = list()
+        for event in self.events:
+            if event.status == "EXCEPTION":
+                errors.append(event)
+        return errors
+
+    def get_latest_event(self) -> TrackingEvent:  # TODO: debug
+        """Returns the latest event to have occurred in the `events` list."""
+        return self.events[-1]
+
+    def has_errors(self) -> bool:  # TODO: debug
+        """Returns `true` if there are any EXCEPTION events."""
+        for event in self.events:
+            return event.status == "EXCEPTION"
+
+    def to_dict(self):
+        return (lambda o: o.__dict__)(self)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=2)
