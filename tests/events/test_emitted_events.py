d62cd4cd4179d1c1db9c6c1ef1ba0e56e561ba9c
KaseyCantu <kpc.software.engineer@gmail.com>
2021-07-16 11:27:48 -0500

    AC per DX-1528 and rename of test file

--- tests/events/test_emitted_events.py
+++ tests/events/test_emitted_events.py
@@ -8,18 +8,19 @@ from shipengine_sdk.events import (
     ShipEngineEventListener,
 )
 from shipengine_sdk.models import Endpoints
+from shipengine_sdk.models.enums import Constants
 
 from ..util import assert_on_429_exception, configurable_stub_shipengine_instance
 
 
-class TestRequestSentEvent:
+class TestEmittedEvents:
     def test_config_with_retries_disabled(self, mocker: MockerFixture) -> None:
         """DX-1527 - Tests that the SDK does not automatically retry if retries in config is set to 0."""
         request_sent_spy = mocker.spy(ShipEngineEventListener, "catch_request_sent_event")
         response_received_spy = mocker.spy(ShipEngineEventListener, "catch_response_received_event")
         shipengine = configurable_stub_shipengine_instance(
             {
-                "api_key": "baz_sim",
+                "api_key": Constants.API_KEY.value,
                 "base_uri": Endpoints.TEST_RPC_URL.value,
                 "retries": 0,
                 "timeout": 10,
@@ -39,28 +40,28 @@ class TestRequestSentEvent:
             assert type(response_recd_return) == ResponseReceivedEvent
             assert response_recd_return.retry == 0
 
-    # def test_config_with_custom_retries(self, mocker: MockerFixture) -> None:
-    #     """DX-1528 - Test config with custom retries."""
-    #     request_sent_spy = mocker.spy(ShipEngineEventListener, "catch_request_sent_event")
-    #     response_received_spy = mocker.spy(ShipEngineEventListener, "catch_response_received_event")
-    #     shipengine = configurable_stub_shipengine_instance(
-    #             {
-    #                 "api_key":  "baz_sim",
-    #                 "base_uri": Endpoints.TEST_RPC_URL.value,
-    #                 "retries":  2,
-    #                 "timeout":  3,
-    #             }
-    #     )
-    #     try:
-    #         shipengine.get_carrier_accounts(carrier_code="amazon_buy_shipping")
-    #     except ShipEngineError as err:
-    #         assert_on_429_exception(err=err, error_class=RateLimitExceededError)
-    #         request_sent_return = request_sent_spy.spy_return
-    #         assert request_sent_spy.call_count == 3
-    #         assert type(request_sent_return) == RequestSentEvent
-    #         assert request_sent_return.retry == 2
-    #
-    #         response_recd_return = response_received_spy.spy_return
-    #         assert request_sent_spy.call_count == 3
-    #         assert type(response_recd_return) == ResponseReceivedEvent
-    #         assert response_recd_return.retry == 2
+    def test_config_with_custom_retries(self, mocker: MockerFixture) -> None:
+        """DX-1528 - Test config with custom retries."""
+        request_sent_spy = mocker.spy(ShipEngineEventListener, "catch_request_sent_event")
+        response_received_spy = mocker.spy(ShipEngineEventListener, "catch_response_received_event")
+        shipengine = configurable_stub_shipengine_instance(
+            {
+                "api_key": Constants.API_KEY.value,
+                "base_uri": Endpoints.TEST_RPC_URL.value,
+                "retries": 3,
+                "timeout": 3,
+            }
+        )
+        try:
+            shipengine.get_carrier_accounts(carrier_code="amazon_buy_shipping")
+        except ShipEngineError as err:
+            assert_on_429_exception(err=err, error_class=RateLimitExceededError)
+            request_sent_return = request_sent_spy.spy_return
+            assert request_sent_spy.call_count == 4
+            assert type(request_sent_return) == RequestSentEvent
+            assert request_sent_return.retry == 3
+
+            response_recd_return = response_received_spy.spy_return
+            assert request_sent_spy.call_count == 4
+            assert type(response_recd_return) == ResponseReceivedEvent
+            assert response_recd_return.retry == 3
